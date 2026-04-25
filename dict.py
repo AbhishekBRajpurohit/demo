873e66e46f62b34f6218d3a4954b6bbbdac6c9a1
Abhishek <abhi@gmail.com>
2026-04-25 16:02:43 +0530

    removed errors

--- dict.py
+++ dict.py
@@ -7,9 +7,3 @@ print(marks,type(marks))
 print(marks["harry"])
 print(marks.items())#it will give output in the form of tupple
 print(marks.keys())
-print(marks.values())
-marks.update({"harry" :90,"renuka": 20})
-print(marks)
-
-print(marks.pop("hui"))
-print(marks)
